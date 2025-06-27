723431bd9526d428cf0a4719deaf9c70ed8bec8f
hamzabdel <hamzaabdel@csu.fullerton.edu>
2025-06-27 14:12:29 -0700

    fixed graph issue

--- agent.py
+++ agent.py
@@ -4,7 +4,6 @@ import numpy as np
 from collections import deque
 from game import SnakeGameAI, Direction, Point
 from model import Linear_QNet, QTrainer
-from helper import plot
 
 MAX_MEMORY = 100_000
 BATCH_SIZE = 1000
@@ -78,8 +77,6 @@ class Agent:
 
         states, actions, rewards, next_states, dones = zip(*mini_sample)
         self.trainer.train_step(states, actions, rewards, next_states, dones)
-        #for state, action, reward, nexrt_state, done in mini_sample:
-        #    self.trainer.train_step(state, action, reward, next_state, done)
 
     def train_short_memory(self, state, action, reward, next_state, done):
         self.trainer.train_step(state, action, reward, next_state, done)
@@ -101,8 +98,6 @@ class Agent:
 
 
 def train():
-    plot_scores = []
-    plot_mean_scores = []
     total_score = 0
     record = 0
     agent = Agent()
@@ -125,7 +120,7 @@ def train():
         agent.remember(state_old, final_move, reward, state_new, done)
 
         if done:
-            # train long memory, plot result
+            # train long memory
             game.reset()
             agent.n_games += 1
             agent.train_long_memory()
@@ -136,11 +131,9 @@ def train():
 
             print('Game', agent.n_games, 'Score', score, 'Record:', record)
 
-            plot_scores.append(score)
             total_score += score
             mean_score = total_score / agent.n_games
-            plot_mean_scores.append(mean_score)
-            plot(plot_scores, plot_mean_scores)
+            print(f'Mean Score: {mean_score:.2f}')
 
 
 if __name__ == '__main__':
